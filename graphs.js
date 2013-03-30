6f0b1a86ebfab7deb6b7a230df9ae0074eb89d7d
Martin Robinson <mrobinson@igalia.com>
2013-03-30 12:33:08 -0700

    Clean up the HTML and split out CSS/JavaScript

---
+++ graphs.js
@@ -0,0 +1,107 @@
+/*
+ * Copyright © 2006 Red Hat, Inc.
+ * Copyright © 2013 Igalia S.L.
+ *
+ * Permission to use, copy, modify, distribute, and sell this software
+ * and its documentation for any purpose is hereby granted without
+ * fee, provided that the above copyright notice appear in all copies
+ * and that both that copyright notice and this permission notice
+ * appear in supporting documentation, and that the name of
+ * the authors not be used in advertising or publicity pertaining to
+ * distribution of the software without specific, written prior
+ * permission. The authors make no representations about the
+ * suitability of this software for any purpose.  It is provided "as
+ * is" without express or implied warranty.
+ *
+ * THE AUTHORS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS
+ * SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS, IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+function findArrayStats(values) {
+    var result = {
+        'standardDeviation': 0,
+        'median': 0,
+        'mean': 0,
+    };
+
+    if (values.length == 0)
+        return result;
+
+    if (values.length == 1) {
+        result.mean = result.median = values[0];
+    }
+
+
+    // First, identify any outliers, using the definition of "mild
+    // outliers" from: http://en.wikipedia.org/wiki/Outliers
+    //
+    // Which is that outliers are any values less than Q1 - 1.5 * IQR
+    // or greater than Q3 + 1.5 * IQR where Q1 and Q3 are the first
+    // and third quartiles and IQR is the inter-quartile range (Q3 -
+    // Q1).
+    values.sort();
+
+    var q1 = values[Math.floor(1 * values.length / 4)];
+    var q3 = values[Math.floor(3 * values.length / 4)];
+
+    var iqr = q3 - q1;
+    var outlier_min = q1 - 3 * iqr / 2;
+    var outlier_max = q3 + 3 * iqr / 2;
+
+    for (var i = 0; i < values.length && values[i] < outlier_min; i++) {}
+    var min_valid = i;
+
+    for (var i = 0; i < values.length && values[i] <= outlier_max; i++) {}
+    var num_valid = i - min_valid;
+
+    var sum = 0;
+    for (var i = min_valid; i < min_valid + num_valid; i++) {
+        sum += values[i];
+    }
+    result.mean = sum / num_valid;
+
+    // Let's use a normalized std. deviation for easier comparison.
+    var s = 0;
+    for (var i = min_valid; i < min_valid + num_valid; i++) {
+        var delta = (values[i] - result.mean) / result.mean;
+        s += delta * delta;
+    }
+    result.standardDeviation = Math.sqrt(s / num_valid);
+    return result;
+}
+
+function processSamples(data) {
+    if (data === undefined)
+        return [];
+    var stats = findArrayStats(data.samples);
+    return [stats.mean / (data.normalization * 1000), stats.standardDeviation];
+}
+
+function graphFromTrace(element, data) {
+    var results = data['results'];
+
+    var series = [];
+    var backends = data['backends'];
+
+    for (var i = 0; i < results.length; i++) {
+        var commit = results[i];
+        var newSeries = [i];
+        for (var backendIndex = 0; backendIndex < backends.length; backendIndex++) {
+            var backend = backends[backendIndex];
+            newSeries.push(processSamples(commit[backend]));
+        }
+
+        series.push(newSeries);
+    }
+    new Dygraph(element, series, 
+                { errorBars: true,
+                  labels: ['commit'].concat(backends),
+                  highlightCallback: function(e, x, points, row) {
+                      document.getElementById('commitmessage').innerText = results[row].message;
+                 }});
+}
